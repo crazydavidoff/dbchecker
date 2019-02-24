ddd01ec05cf2b2f836672068f7672396cdfc1d16
crazydavidoff <artyom.davydov@gmail.com>
2019-02-24 20:37:21 +0300

    researchs

--- checker.py
+++ checker.py
@@ -1,1 +1,25 @@
-print('hello world')
+import mysql.connector
+
+userdb = mysql.connector.connect(
+    host = "172.16.20.38",
+    user = "",
+    passwd = "",
+    db = "userdb"
+)
+
+userdbcursor = userdb.cursor()
+
+userdbcursor.execute("SELECT login,password FROM users")
+
+result = userdbcursor.fetchall()
+print(result)
+
+if not result:
+    print("Table is empty")
+    exit(0)
+
+else:
+
+
+    for row in result:
+        print(row)
