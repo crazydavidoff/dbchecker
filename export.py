55408a67b1c8dc7f61c6745b37e3b22a91c26dc5
crazydavidoff <artyom.davydov@gmail.com>
2019-02-25 14:46:56 +0300

    some changes

--- export.py
+++ export.py
@@ -1,16 +1,47 @@
 import mysql.connector
+import getpass
 
-userdb = mysql.connector.connect(
-    host = "172.16.20.38",
-    user = "",
-    passwd = "",
-    db = "userdb"
-)
+hosts = open("hosts.txt")
 
-userdbcursor = userdb.cursor()
+login = input("Login: ")
+passwd = getpass.getpass('Password: ')
 
-userdbcursor.execute("select User,Password from mysql.user where Host like '172.16%'")
+for hostdb in hosts:
 
-result = userdbcursor.fetchall()
-for row in result:
-    print(row)
+    hostdb = hostdb[:-1]
+
+    exportdb = mysql.connector.connect(
+        host = hostdb,
+        user = login,
+        passwd = passwd
+    )
+    exportdbcursor = exportdb.cursor()
+
+    exportdbcursor.execute("show variables like 'hostname'")
+    hostname = exportdbcursor.fetchone()
+    exportdbcursor.execute("show variables like 'innodb_version'")
+    version = exportdbcursor.fetchone()
+
+    exportdbcursor.execute("select User,Password from mysql.user where Host like '172.16%'")
+
+    result = exportdbcursor.fetchall()
+
+    for row in result:
+        row = (hostname[1], ) + (hostdb, ) + row + (version[1], )
+
+        userdb = mysql.connector.connect(
+            host="172.16.20.38",
+            user=login,
+            passwd=passwd,
+            db="userdb"
+        )
+        userdbcursor = userdb.cursor()
+
+        sql_insert = "INSERT INTO users (hostname,ip,login,password,version) VALUES (%s, %s, %s, %s, %s)"
+
+        userdbcursor.execute(sql_insert, row)
+        userdb.commit()
+        print(row)
+
+hosts.close()
+
